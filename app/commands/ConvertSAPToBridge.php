7a392f0c35617a33fbabf4ed818cc66e32d02002
Uice Lu <uicestone@gmail.com>
2015-02-15 10:51:25 +0800

    enhance Inbound stablility

--- app/commands/ConvertSAPToBridge.php
+++ app/commands/ConvertSAPToBridge.php
@@ -57,77 +57,83 @@ class ConvertSAPToBridge extends Command {
 		
 		foreach($list as $path)
 		{
-			if(File::extension($path) !== 'xls' && File::extension($path) !== 'xlsx'){
-				continue;
-			}
+			$this->info($path);
 			
-			Excel::load($path, function($reader) use($config, $path)
-			{
-				$result = $reader->noHeading()->toArray();
+			try{
+				
+				if(strtolower(File::extension($path)) !== 'xls' && strtolower(File::extension($path)) !== 'xlsx'){
+					continue;
+				}
 
-				$data = array();
+				Excel::selectSheetsByIndex(0)->load($path, function($reader) use($config, $path)
+				{
+					$result = $reader->noHeading()->toArray();
 
-				foreach($result[0] as $sheet_line){
+					$data = array();
 
-					if(strtolower(trim($sheet_line[12])) !== 'success' || !$sheet_line[4]){
-						continue;
+					foreach($result as $sheet_line){
+						
+						if(strtolower(trim($sheet_line[12])) !== 'success' || !$sheet_line[4]){
+							continue;
+						}
+						$data[] = $sheet_line;
 					}
-					$data[] = $sheet_line;
-				}
 
-				$output = '100,FL,ID' . "\n";
+					$output = '100,FL,ID' . "\n";
 
-				foreach($data as $item){
-					$line_data = array(
-						600,
-						$item[6] > 0 ? - round($item[6] * 100) : round($item[6] * 100), // Amount
-						method_exists($item[10], 'format') ? $item[10]->format('Ymd') : date('Ymd',strtotime($item[10])),
-						$item[8] === 'D' ? null : ($item[8] === 'E' ? 'ICBC-corporate' : 'ICBC'),
-						null,
-						$item[7], // Document No.
-						$item[4], // Report ID
-						$item[5], // Currency
-						null,
-						null,
-						null,
-						null,
-						null,
-					);
-					$output .= implode(',', $line_data) . "\n";
-					
-					$last_item_date = method_exists($item[10], 'format') ? $item[10]->format('Ymd') : date('Ymd',strtotime($item[10]));
-					
-				}
+					foreach($data as $index => $item){
+						
+						$line_data = array(
+							600,
+							$item[6] > 0 ? - round($item[6] * 100) : round($item[6] * 100), // Amount
+							method_exists($item[9], 'format') ? $item[9]->format('Ymd') : date('Ymd',strtotime(str_replace('.', '/', $item[9]))),
+							$item[8] === 'D' ? null : ($item[8] === 'E' ? 'ICBC-corporate' : 'ICBC'),
+							null,
+							$item[7], // Document No.
+							$item[4], // Report ID
+							$item[5], // Currency
+							null,
+							null,
+							null,
+							null,
+							null,
+						);
+						$output .= implode(',', $line_data) . "\n";
 
-				$export_file_name = 'PYMT_CONFIRM_' . $last_item_date . '.txt';
-				
-				file_put_contents(storage_path('exports') . '/' . $export_file_name, $output);
-				
-				$this->comment(date('Y-m-d H:i:s') . ' ' . $export_file_name . ' converted, uploading to FTP server...');
-				
-				if(empty($this->ftp_connection)){
-					$this->comment(Date('Y-m-d H:i:s') . ' Logging FTP...');
-					$this->ftp_connection = ftp_connect($config['host'], 21);
-					ftp_pasv($this->ftp_connection, true);
-					ftp_login($this->ftp_connection, $config['username'], $config['password']);
-					$this->info(Date('Y-m-d H:i:s') . ' Logged into FTP server.');
-				}
-				
-				for($i = 0; $i < 20 && !@ftp_pasv($this->ftp_connection, true); $i ++){
-					$this->error(Date('Y-m-d H:i:s') . ' Failed to switch to passive mode. ' . ($i === 19 ? 'Aborting.' : 'Trying again...'));
-				}
-				
-				for($i = 0; $i < 20 && !@ftp_put($this->ftp_connection, $config['path'] . '/' . $export_file_name, storage_path('exports') . '/' . $export_file_name, FTP_BINARY); $i ++){
-					$this->error(Date('Y-m-d H:i:s') . ' Failed to write to FTP server. ' . ($i === 19 ? 'Aborting.' : 'Trying again...'));
-				}
-				
-				File::delete($path);
-				File::delete(storage_path('exports') . '/' . $export_file_name);
-				
-				$this->info(date('Y-m-d H:i:s') . ' ' . $export_file_name . ' uploaded to FTP server.');
-				
-			});
+					}
+
+					$export_file_name = 'PYMT_CONFIRM_' . basename($path, 'xlsx') . '.txt';
+
+					file_put_contents(storage_path('exports') . '/' . $export_file_name, $output);
+
+					$this->comment(date('Y-m-d H:i:s') . ' ' . $export_file_name . ' converted, uploading to FTP server...');
+
+					if(empty($this->ftp_connection)){
+						$this->comment(Date('Y-m-d H:i:s') . ' Logging FTP...');
+						$this->ftp_connection = ftp_connect($config['host'], 21);
+						ftp_pasv($this->ftp_connection, true);
+						ftp_login($this->ftp_connection, $config['username'], $config['password']);
+						$this->info(Date('Y-m-d H:i:s') . ' Logged into FTP server.');
+					}
+
+					for($i = 0; $i < 20 && !@ftp_pasv($this->ftp_connection, true); $i ++){
+						$this->error(Date('Y-m-d H:i:s') . ' Failed to switch to passive mode. ' . ($i === 19 ? 'Aborting.' : 'Trying again...'));
+					}
+
+					for($i = 0; $i < 20 && !@ftp_put($this->ftp_connection, $config['path'] . '/' . $export_file_name, storage_path('exports') . '/' . $export_file_name, FTP_BINARY); $i ++){
+						$this->error(Date('Y-m-d H:i:s') . ' Failed to write to FTP server. ' . ($i === 19 ? 'Aborting.' : 'Trying again...'));
+					}
+
+					File::delete($path);
+					File::delete(storage_path('exports') . '/' . $export_file_name);
+
+					$this->info(date('Y-m-d H:i:s') . ' ' . $export_file_name . ' uploaded to FTP server.');
+
+				});
 			
+			} catch (Exception $ex) {
+				$this->error($ex->getMessage());
+			}
 		}
 		
 		$this->info(date('Y-m-d H:i:s') . ' Completed. (in ' . round(microtime(true) - $start, 3) . 's)');
