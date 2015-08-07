d65dff5677b4fb4cbae49ed4c0d49a1010a2f644
Uice Lu <uicestone@gmail.com>
2015-08-08 00:02:13 +0800

    test read page for SOI data

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -4,6 +4,11 @@ class TestController extends BaseController {
 	
 	function index()
 	{
+		$soi_data = DB::connection('soi')->table('V_ELEAVE_SOI_BASIC_DATA')->get();
 		
+		foreach($soi_data as $soi_row)
+		{
+			print_r($soi_row);
+		}
 	}
 }
