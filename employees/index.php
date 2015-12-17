b28114c8b6aa82612da72ad8d348400334278721
walterwan90 <walterwan1990@gmail.com>
2015-12-17 14:29:59 +0800

    Re-structure data from SOI

--- employees/index.php
+++ employees/index.php
@@ -14,62 +14,91 @@
         or die('Error in Connecting: ' . mysqli_error($con));
 
     // use prepare statement for insert query
-    $st = mysqli_prepare($con, 'INSERT INTO employees(id, `Company Code`, Company, `ID Number`, `Employee Name`, `Last Name`, `First Name`, `User Name`, `E-Mail Address`, `Local Grade`, `Cost Center`, `Direct Manager ID`, `Direct Manager`, `Department Head ID`, `Department Head`, `Department`, `Update Date`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-        or die(mysqli_error($con));
-    $update = mysqli_prepare($con, 'INSERT INTO employees_updated(id, `Company Code`, Company, `ID Number`, `Employee Name`, `Last Name`, `First Name`, `User Name`, `E-Mail Address`, `Local Grade`, `Cost Center`, `Direct Manager ID`, `Direct Manager`, `Department Head ID`, `Department Head`, `Department`, `Update Date`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-        or die(mysqli_error($con));
-    $wp_users = mysqli_prepare($con, 'INSERT INTO wp_users(ID, user_login, user_email, user_nicename, display_name, company_code, company, id_number, last_name, first_name, logon_id, local_grade, cost_center, direct_manager_id, direct_manager, department_head_id, department_head, department, update_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-        or die(mysqli_error($con));
-    
+    $st = mysqli_prepare($con, 'INSERT INTO _soi_data(PERNR, STAT2, ZZHIRE_DATE, ZZTERM_DATE, BUKRS, PERSG, ZZPERSG_TXT, BUTXT, KOSTL, WERKS, ZZWERKS_TXT, ZZDEP_OM, ZZDEP_OM_TXT, ZZHRMAN_VORNA, ZZHRMAN_NACHN, ZZHRMAN_PERNR, ZZHRMAN_USERID, ZZHRMAN_MAIL, ZZHRREP_VORNA, ZZHRREP_NACHN, ZZHRREP_PERNR, ZZCHIEF_DEP_VORN, ZZCHIEF_DEP_NACH, ZZCHIEF_DEP_PERN, NACHN, VORNA, GESCH, ZZGESCH_TXT, ZZMAIL, ZZUSERID, FATXT, ZZLEGACY_ID, ZZLCGDE, ZZUPD_DATE, ZZTYPE_UPD, ZZPERID) 
+                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+        or die("Error in Preparing: " . mysqli_error($con));
+    $update = mysqli_prepare($con, 'INSERT INTO _soi_data_updated(PERNR, STAT2, ZZHIRE_DATE, ZZTERM_DATE, BUKRS, PERSG, ZZPERSG_TXT, BUTXT, KOSTL, WERKS, ZZWERKS_TXT, ZZDEP_OM, ZZDEP_OM_TXT, ZZHRMAN_VORNA, ZZHRMAN_NACHN, ZZHRMAN_PERNR, ZZHRMAN_USERID, ZZHRMAN_MAIL, ZZHRREP_VORNA, ZZHRREP_NACHN, ZZHRREP_PERNR, ZZCHIEF_DEP_VORN, ZZCHIEF_DEP_NACH, ZZCHIEF_DEP_PERN, NACHN, VORNA, GESCH, ZZGESCH_TXT, ZZMAIL, ZZUSERID, FATXT, ZZLEGACY_ID, ZZLCGDE, ZZUPD_DATE, ZZTYPE_UPD, ZZPERID) 
+                                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+        or die("Error in Preparing: " . mysqli_error($con));
+  /*  $wp_users = mysqli_prepare($con, 'INSERT IGNORE INTO wp_users(ID, user_login, user_email, user_nicename, display_name, employee_id, company_code, company, id_number, last_name, first_name, logon_id, local_grade, cost_center, direct_manager_id, direct_manager, department_head_id, department_head, department, update_date) 
+   *                                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+   *     or die("Error in Preparing: " . mysqli_error($con));
+   */
+
     // bind variables to insert query params
-    mysqli_stmt_bind_param($st, 'issssssssisisisss', $id, $company_code, $company, $id_number, $employee_name, $last_name, $first_name, $logon_id, $email, $local_grade, $cost_center, $manager_id, $manager, $chief_id, $chief, $department, $update_date);
-    mysqli_stmt_bind_param($update, 'issssssssisisisss', $id, $company_code, $company, $id_number, $employee_name, $last_name, $first_name, $logon_id, $email, $local_grade, $cost_center, $manager_id, $manager, $chief_id, $chief, $department, $update_date);
-    mysqli_stmt_bind_param($wp_users, 'issssssssssisisisss', $id, $email, $email, $employee_name, $employee_name, $company_code, $company, $id_number, $last_name, $first_name, $logon_id, $local_grade, $cost_center, $manager_id, $manager, $chief_id, $chief, $department, $update_date);
+    mysqli_stmt_bind_param($st, 'issssssssssssssissssissississsssssss', $id, $stat2, $hire_date, $term_date, $company_code, $persg, $persg_txt, $company, $cost_center, $werks, $site, $dep_om, $department, $manager_firnm, $manager_lasnm, $manager_id, $manager_userid, $manager_email, $hr_firnm, $hr_lasnm, $hr_id, $chief_firnm, $chief_lasnm, $chief_id, $last_name, $first_name, $gesch, $gender, $email, $logon_id, $fatxt, $legacy_id, $local_grade, $update_date, $type_upd, $id_number)
+        or die("Error in banding: " . mysqli_error($con));
+    mysqli_stmt_bind_param($update, 'issssssssssssssissssissississsssssss', $id, $stat2, $hire_date, $term_date, $company_code, $persg, $persg_txt, $company, $cost_center, $werks, $site, $dep_om, $department, $manager_firnm, $manager_lasnm, $manager_id, $manager_userid, $manager_email, $hr_firnm, $hr_lasnm, $hr_id, $chief_firnm, $chief_lasnm, $chief_id, $last_name, $first_name, $gesch, $gender, $email, $logon_id, $fatxt, $legacy_id, $local_grade, $update_date, $type_upd, $id_number)
+        or die("Error in banding: " . mysqli_error($con));   
+    // mysqli_stmt_bind_param($wp_users, 'isssssssssssssisisss', $id, $email, $email, $employee_name, $employee_name, $id, $company_code, $company, $id_number, $last_name, $first_name, $logon_id, $local_grade, $cost_center, $manager_id, $manager, $chief_id, $chief, $department, $update_date)
+    //     or die("Error in banding: " . mysqli_error($con));
     
     //convert json object to php associative array
     $data = json_decode($json, true);
     $data_old = json_decode($json_old, true);
     $diff = array_diff(array_recursive($data, $data_rt), array_recursive($data_old, $data_old_rt));
-    
-    print_r($diff);
 
     // loop through the array
     foreach ($data as $row) {
         // get the employee details
         $id = $row['PERNR'];
+        $stat2 = $row['STAT2'];
+        $hire_date = $row['ZZHIRE_DATE'];
+        $term_date = $row['ZZTERM_DATE'];
         $company_code = $row['BUKRS'];
+        $persg = $row['PERSG'];
+        $persg_txt = $row['ZZPERSG_TXT'];
         $company = $row['BUTXT'];
+        $cost_center = $row['KOSTL'];
+        $werks = $row['WERKS'];
+        $site = $row['ZZWERKS_TXT'];
+        $dep_om = $row['ZZDEP_OM'];
+        $department = $row['ZZDEP_OM_TXT'];
+        $manager_firnm = $row['ZZHRMAN_VORNA'];
+        $manager_lasnm = $row['ZZHRMAN_NACHN'];
+        $manager_id = $row['ZZHRMAN_PERNR'];
+        $manager_userid = $row['ZZHRMAN_USERID'];
+        $manager_email = $row['ZZHRMAN_MAIL'];
+        $hr_firnm = $row['ZZHRREP_VORNA'];
+        $hr_lasnm = $row['ZZHRREP_NACHN'];
+        $hr_id = $row['ZZHRREP_PERNR'];
+        $chief_firnm = $row['ZZCHIEF_DEP_VORN'];
+        $chief_lasnm = $row['ZZCHIEF_DEP_NACH'];
+        $chief_id = $row['ZZCHIEF_DEP_PERN'];
+        $last_name = ucwords(strtolower($row['NACHN']));        
+        $first_name = ucwords(strtolower($row['VORNA']));
+        $gesch = $row['GESCH'];
+        $gender = $row['ZZGESCH_TXT'];
+        $email = strtolower($row['ZZMAIL']);
+        $logon_id = $row['ZZUSERID'];
+        $fatxt = $row['FATXT'];
+        $legacy_id = $row['ZZLEGACY_ID'];
+        $local_grade = $row['ZZLCGDE'];
+        $update_date = $row["ZZUPD_DATE"];
+        $type_upd = $row['ZZTYPE_UPD'];
         $id_number = $row['ZZPERID'];
         $employee_name = ucwords(strtolower($row['VORNA'] . ' ' . $row['NACHN']));
-        $last_name = ucwords(strtolower($row['NACHN']));
-        $first_name = ucwords(strtolower($row['VORNA']));
-        $logon_id = $row['ZZUSERID'];
-        $email = strtolower($row['ZZMAIL']);
-        $local_grade = $row['ZZLCGDE'];
-        $cost_center = $row['KOSTL'];
-        $manager_id = $row['ZZHRMAN_PERNR'];
         $manager = ucwords(strtolower($row['ZZHRMAN_VORNA'] . ' ' . $row['ZZHRMAN_NACHN']));
-        $department = $row['ZZDEP_OM_TXT'];
-        $chief_id = $row["ZZCHIEF_DEP_PERN"];
         $chief = ucwords(strtolower($row["ZZCHIEF_DEP_VORN"] . ' ' . $row["ZZCHIEF_DEP_NACH"]));
-        $update_date = $row["ZZUPD_DATE"];
-
+        
+        
         foreach ($diff as $row2) {
-          // $update_date2 = $row2["ZZUPD_DATE"];
-          //  if ($update_date2 == $update_date) {
-          //      mysqli_stmt_execute($update);
+
             if ($row2 == $update_date) {
                 mysqli_stmt_execute($update);
             }
         }
-        mysqli_stmt_execute($wp_users);
-        mysqli_stmt_execute($st);  
+        // mysqli_stmt_execute($wp_users)
+        //     or die("Error in executing: " . mysqli_error($con));
+        mysqli_stmt_execute($st)
+            or die("Error in executing: " . mysqli_error($con));  
     }
 
     
     //close connection
     mysqli_close($con);
 
+    echo "Succeed!";
 /*
 function array_diff_assoc_recursive($array1,$array2){  
     $diffarray=array();  
