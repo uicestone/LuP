cd9c49398e28697c38921efd1729a3cf04a7e62a
walterwan90 <walterwan1990@gmail.com>
2016-01-21 13:53:14 +0800

    [Employee Import]

--- employees/index.php
+++ employees/index.php
@@ -20,18 +20,12 @@
     $update = mysqli_prepare($con, 'INSERT INTO _soi_data_updated(PERNR, STAT2, ZZHIRE_DATE, ZZTERM_DATE, BUKRS, PERSG, ZZPERSG_TXT, BUTXT, KOSTL, WERKS, ZZWERKS_TXT, ZZDEP_OM, ZZDEP_OM_TXT, ZZHRMAN_VORNA, ZZHRMAN_NACHN, ZZHRMAN_PERNR, ZZHRMAN_USERID, ZZHRMAN_MAIL, ZZHRREP_VORNA, ZZHRREP_NACHN, ZZHRREP_PERNR, ZZCHIEF_DEP_VORN, ZZCHIEF_DEP_NACH, ZZCHIEF_DEP_PERN, NACHN, VORNA, GESCH, ZZGESCH_TXT, ZZMAIL, ZZUSERID, FATXT, ZZLEGACY_ID, ZZLCGDE, ZZUPD_DATE, ZZTYPE_UPD, ZZPERID) 
                                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
         or die("Error in Preparing: " . mysqli_error($con));
-  /*  $wp_users = mysqli_prepare($con, 'INSERT IGNORE INTO wp_users(ID, user_login, user_email, user_nicename, display_name, employee_id, company_code, company, id_number, last_name, first_name, logon_id, local_grade, cost_center, direct_manager_id, direct_manager, department_head_id, department_head, department, update_date) 
-   *                                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-   *     or die("Error in Preparing: " . mysqli_error($con));
-   */
 
     // bind variables to insert query params
-    mysqli_stmt_bind_param($st, 'issssssssssssssissssissississsssssss', $id, $stat2, $hire_date, $term_date, $company_code, $persg, $persg_txt, $company, $cost_center, $werks, $site, $dep_om, $department, $manager_firnm, $manager_lasnm, $manager_id, $manager_userid, $manager_email, $hr_firnm, $hr_lasnm, $hr_id, $chief_firnm, $chief_lasnm, $chief_id, $last_name, $first_name, $gesch, $gender, $email, $logon_id, $fatxt, $legacy_id, $local_grade, $update_date, $type_upd, $id_number)
+    mysqli_stmt_bind_param($st, 'issssssssssssssissssississssssssssss', $id, $stat2, $hire_date, $term_date, $company_code, $persg, $persg_txt, $company, $cost_center, $werks, $site, $dep_om, $department, $manager_firnm, $manager_lasnm, $manager_id, $manager_userid, $manager_email, $hr_firnm, $hr_lasnm, $hr_id, $chief_firnm, $chief_lasnm, $chief_id, $last_name, $first_name, $gesch, $gender, $email, $logon_id, $fatxt, $legacy_id, $local_grade, $update_date, $type_upd, $id_number)
         or die("Error in banding: " . mysqli_error($con));
-    mysqli_stmt_bind_param($update, 'issssssssssssssissssissississsssssss', $id, $stat2, $hire_date, $term_date, $company_code, $persg, $persg_txt, $company, $cost_center, $werks, $site, $dep_om, $department, $manager_firnm, $manager_lasnm, $manager_id, $manager_userid, $manager_email, $hr_firnm, $hr_lasnm, $hr_id, $chief_firnm, $chief_lasnm, $chief_id, $last_name, $first_name, $gesch, $gender, $email, $logon_id, $fatxt, $legacy_id, $local_grade, $update_date, $type_upd, $id_number)
+    mysqli_stmt_bind_param($update, 'issssssssssssssissssississssssssssss', $id, $stat2, $hire_date, $term_date, $company_code, $persg, $persg_txt, $company, $cost_center, $werks, $site, $dep_om, $department, $manager_firnm, $manager_lasnm, $manager_id, $manager_userid, $manager_email, $hr_firnm, $hr_lasnm, $hr_id, $chief_firnm, $chief_lasnm, $chief_id, $last_name, $first_name, $gesch, $gender, $email, $logon_id, $fatxt, $legacy_id, $local_grade, $update_date, $type_upd, $id_number)
         or die("Error in banding: " . mysqli_error($con));   
-    // mysqli_stmt_bind_param($wp_users, 'isssssssssssssisisss', $id, $email, $email, $employee_name, $employee_name, $id, $company_code, $company, $id_number, $last_name, $first_name, $logon_id, $local_grade, $cost_center, $manager_id, $manager, $chief_id, $chief, $department, $update_date)
-    //     or die("Error in banding: " . mysqli_error($con));
     
     //convert json object to php associative array
     $data = json_decode($json, true);
@@ -88,8 +82,6 @@
                 mysqli_stmt_execute($update);
             }
         }
-        // mysqli_stmt_execute($wp_users)
-        //     or die("Error in executing: " . mysqli_error($con));
         mysqli_stmt_execute($st)
             or die("Error in executing: " . mysqli_error($con));  
     }
